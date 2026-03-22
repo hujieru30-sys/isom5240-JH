de1b0aab3cf43b7d551683102b46f7a118e3d7c8
hujieru30-sys <hujieru30@gmail.com>
2026-03-22 18:30:57 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -1,21 +1,17 @@
 import streamlit as st
 from transformers import pipeline
 
-def main():
-    sentiment_pipeline = pipeline(model="JR-2026/CustomModel_medical"")
+@st.cache_resource
+def load_model():
+    return pipeline("text-classification", model="JR-2026/CustomModel_medical")
 
-    st.title("Sentiment Analysis with HuggingFace Spaces")
-    st.write("Enter a sentence to analyze its sentiment:")
+st.title("medical classification")
+user_input = st.text_input("Describe the condition of the illness：")
 
-    user_input = st.text_input("")
-    if user_input:
-        result = sentiment_pipeline(user_input)
-        sentiment = result[0]["label"]
-        confidence = result[0]["score"]
-
-        st.write(f"Sentiment: ")
-        for r in result:
-            st.write(f"{r['label']}: {r['score']:.4f}")
-
-if __name__ == "__main__":
-    main()
+if user_input:
+    classifier = load_model()
+    result = classifier(user_input)
+    
+    st.write("预测结果：")
+    for res in result:
+        st.write(f"{res['label']}: {res['score']:.4f}")
